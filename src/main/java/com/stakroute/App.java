4c193c6e0347a6b7566c441b1e415a302c5e3f17
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-07-29 15:53:50 +0530

    Initial commit.

---
+++ src/main/java/com/stakroute/App.java
@@ -0,0 +1,13 @@
+package com.stakroute;
+
+/**
+ * Spring - Annotations Tasks
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
