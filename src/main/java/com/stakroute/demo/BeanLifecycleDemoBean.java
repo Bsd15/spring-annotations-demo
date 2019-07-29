0abfec4ef2e7670a07e354527b3dbda3f9e71cc3
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-07-29 22:59:54 +0530

    Task - 6 Completed.

--- src/main/java/com/stakroute/demo/BeanLifecycleDemoBean.java
+++ src/main/java/com/stakroute/demo/BeanLifecycleDemoBean.java
@@ -8,13 +8,16 @@ import javax.annotation.PreDestroy;
 @Component
 public class BeanLifecycleDemoBean {
     /**
-     * The method is invoked after initializing the bean
+     * The method is invoked after initializing the bean.
      */
     @PostConstruct
     private void customInit() {
         System.out.println("customInit(): After Initializing bean.");
     }
 
+    /**
+     * The method is invoked before destroying the bean.
+     */
     @PreDestroy
     private void customDestroy() {
         System.out.println("customDestroy(): Before destroying the bean.");
