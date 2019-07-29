86f750eb7b99f1a9b8684cb43606e1e0bba716ce
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-07-29 22:46:23 +0530

    Task - 4 Completed.

--- src/main/java/com/stakroute/domain/Movie.java
+++ src/main/java/com/stakroute/domain/Movie.java
@@ -1,10 +1,16 @@
 package com.stakroute.domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Movie {
+public class Movie implements ApplicationContextAware, BeanNameAware, BeanFactoryAware {
     @Autowired
     private Actor actor;
 
@@ -21,5 +27,20 @@ public class Movie {
         return "Movie{" +
                 "actor=" + actor +
                 '}';
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Movie BeanFactory: " + beanFactory.toString());
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Movie BeanName: " + name);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Movie ApplicationContext: " + applicationContext.toString());
     }
 }
