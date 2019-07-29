4822c4a40a80ceecfbb02322a671e8f0459a03e8
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-07-29 16:10:06 +0530

    Task - 1 Complete.

---
+++ src/main/java/com/stakroute/domain/Movie.java
@@ -0,0 +1,27 @@
+package com.stakroute.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Movie {
+    private Actor actor;
+
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "actor=" + actor +
+                '}';
+    }
+}
