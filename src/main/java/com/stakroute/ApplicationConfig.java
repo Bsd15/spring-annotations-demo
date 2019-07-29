d27f0f61fe793453d5169358c67dcb222339ae5e
sivadatta.bathina <sivadatta.bathina@cgi.com>
2019-07-29 22:06:30 +0530

    Task - 2 Complete.

--- src/main/java/com/stakroute/ApplicationConfig.java
+++ src/main/java/com/stakroute/ApplicationConfig.java
@@ -4,6 +4,7 @@ import com.stakroute.domain.Actor;
 import com.stakroute.domain.Movie;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class ApplicationConfig {
@@ -13,7 +14,23 @@ public class ApplicationConfig {
     }
 
     @Bean
+    public Actor actor2() {
+        return new Actor("Actor 2", "Female", 29);
+    }
+
+    @Bean
+    public Actor actor3() {
+        return new Actor("Actor 3", "Male", 54);
+    }
+
+    @Bean
     public Movie movie() {
+        return new Movie(actor());
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Movie prototypeScopeMovie() {
         return new Movie(actor());
     }
 }
